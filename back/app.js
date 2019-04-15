97ab454a159cadda4a26e0a6dadbc5df7e9a8678
Andryut <aahuertasca@unal.edu.co>
2019-04-14 19:29:15 -0500

    cors enabled

--- back/app.js
+++ back/app.js
@@ -2,11 +2,13 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index.js');
 
 var app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- back/routes/index.js
+++ back/routes/index.js
@@ -1,20 +1,22 @@
 var express = require('express');
-var router = express.Router();
+var cors = require('cors');
 
+var config = require('../config/config.js');
 let authentication = require('../auth/authentication.js');
 let authorization = require('../auth/authorization.js');
 let user = require('../controllers/user.js');
 
-let home = (req, res) => {
-  res.json({
-    success: true,
-    message: 'Home page'
-  });
-}
+var router = express.Router();
+
+// Example using cors: 
+/*
+app.get('/products/:id', cors(config.corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for only example.com.'})
+});
+*/
 
 router.post('/login', authentication.createToken);
 router.post('/signup', user.toRegister);
 router.post('/signout', authorization.checkToken, authentication.deleteToken);
-router.get('/home', authorization.checkToken, home);
 
 module.exports = router;
