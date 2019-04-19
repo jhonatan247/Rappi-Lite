9b37dba80ee57ea2da38fe2dc8270a2900f087e3
Jhonatan Torres <yon.t.f@hotmail.com>
2019-04-19 02:52:42 -0500

    merge

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var cors = require('cors');
+
+var config = require('../config/config.js');
+let authentication = require('../auth/authentication.js');
+let authorization = require('../auth/authorization.js');
+let user = require('../controllers/user.js');
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Example using cors:
+/*
+app.get('/products/:id', cors(config.corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for only example.com.'})
 });
+
+*/
+// User
+router.post('/login', authentication.createToken);
+router.post('/signup', user.register);
+router.post('/signout', authorization.checkToken, authentication.deleteToken);
 
 module.exports = router;
