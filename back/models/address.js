cf5b6cc8328af822119beb608ebe926a9bb21ad5
Christian Torres <cctorresr@unal.edu.co>
2019-04-24 00:21:28 -0500

    save address

--- back/models/address.js
+++ back/models/address.js
@@ -2,7 +2,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Address = sequelize.define('Address', {
     user_id: DataTypes.INTEGER,
-    value: DataTypes.STRING
+    restaurant_id: DataTypes.INTEGER,
+    value: DataTypes.STRING,
+    latitude: DataTypes.DOUBLE,
+    longitude: DataTypes.DOUBLE
   }, {});
   Address.associate = function(models) {
     Address.belongsTo(models.User);
--- back/routes/index.js
+++ back/routes/index.js
@@ -6,6 +6,8 @@ let authentication = require('../auth/authentication.js');
 let authorization = require('../auth/authorization.js');
 let user = require('../controllers/user.js');
 
+let address = require('../controllers/address.js');
+
 var router = express.Router();
 
 // Example using cors:
@@ -19,5 +21,7 @@ app.get('/products/:id', cors(config.corsOptions), function (req, res, next) {
 router.post('/login', authentication.createToken);
 router.post('/signup', user.register);
 router.post('/signout', authorization.checkToken, authentication.deleteToken);
+
+router.post('/address', address.save);
 
 module.exports = router;
